32dfff165c61fa2df71b86efc49d86aa693fee18
yann marquet <yann.marquet@free.fr>
2010-08-19 18:43:35 +0200

    use new typedData in the C api, faster.

--- ext/rcoremidi/Client.h
+++ ext/rcoremidi/Client.h
@@ -7,6 +7,17 @@ VALUE dispose_client(VALUE self);
 VALUE cClientAlloc(VALUE klass);
 VALUE start(VALUE self);
 VALUE stop(VALUE stop);
+VALUE client_alloc();
+
+typedef struct RCoremidiNode
+{
+    MIDIClientRef *client;
+    char *name;
+    MIDIPortRef *in;
+    MIDIPortRef *out;
+} RCoremidiNode;
+
+
 #endif
 
 
