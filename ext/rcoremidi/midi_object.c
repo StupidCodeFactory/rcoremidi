fadd4cfc1a5eb26f06591a646fff5e3e0613a43f
Yann Marquet <ymarquet@gmail.com>
2014-04-30 04:47:18 +0100

    got the entities to be correctly populated in the device

--- ext/rcoremidi/midi_object.c
+++ ext/rcoremidi/midi_object.c
@@ -40,14 +40,13 @@ set_name_from_properties(CFPropertyListRef *midi_properties, CFStringRef *name)
         *name = CFDictionaryGetValue(*midi_properties, CFSTR("name"));
 }
 
-/* static void * get_entity(CFArrayRef * entities, CFIndex *index) */
-/* { */
-/*         CFArrayGetValueAtIndex() */
-/* } */
-
 static VALUE
-create_entity_from_properties(CFPropertyListRef midi_properties)
+create_entity_obj(MIDIEntityRef *midi_object)
 {
+        OSStatus error;
+        CFPropertyListRef midi_properties;
+        error = MIDIObjectGetProperties(*midi_object, &midi_properties, true);
+
         CFStringRef name;
         CFNumberRef uniqueID;
         SInt64 uid;
@@ -55,46 +54,56 @@ create_entity_from_properties(CFPropertyListRef midi_properties)
         set_name_from_properties(&midi_properties, &name);
         set_uid_from_properties(&midi_properties, &uniqueID, &uid);
 
-        VALUE midi_object = rb_funcall(rb_cEntity, new_intern, 0);
+        VALUE midi_entity = rb_funcall(rb_cEntity, new_intern, 0);
 
-        rb_iv_set(midi_object, "@name", rb_str_new2(CFStringGetCStringPtr(name, kCFStringEncodingUTF8)));
-        rb_iv_set(midi_object, "@uid", INT2FIX(uid));
+        rb_iv_set(midi_entity, "@name", rb_str_new2(CFStringGetCStringPtr(name, kCFStringEncodingUTF8)));
+        rb_iv_set(midi_entity, "@uid", INT2FIX(uid));
 
-        return midi_object;
+        return midi_entity;
 }
 
 static VALUE
-create_source_from_properties(CFPropertyListRef *midi_properties)
+create_source_obj(MIDIEndpointRef *midi_object)
 {
+
+        OSStatus error;
+        CFPropertyListRef midi_properties;
+        error = MIDIObjectGetProperties(*midi_object, &midi_properties, true);
+
         CFNumberRef uniqueID;
         SInt64 uid;
-        set_uid_from_properties(midi_properties, &uniqueID, &uid);
+        set_uid_from_properties(&midi_properties, &uniqueID, &uid);
 
-        VALUE midi_object = rb_funcall(rb_cSource, new_intern, 0);
-        rb_iv_set(midi_object, "@uid", INT2FIX(uid));
+        VALUE midi_source = rb_funcall(rb_cSource, new_intern, 0);
+        rb_iv_set(midi_source, "@uid", INT2FIX(uid));
 
-        return midi_object;
+        return midi_source;
 }
 
 static VALUE
-create_destination_from_properties(CFPropertyListRef *midi_properties)
-{
-        CFNumberRef uniqueID;
-        SInt64 uid;
-        set_uid_from_properties(midi_properties, &uniqueID, &uid);
-
-        VALUE midi_object = rb_funcall(rb_cDestination, new_intern, 0);
-        rb_iv_set(midi_object, "@uid", INT2FIX(uid));
-
-        return midi_object;
-}
-
-static VALUE
-create_device_from_properties(MIDIDeviceRef *midi_device)
+create_destination_obj(MIDIEndpointRef *midi_object)
 {
         OSStatus error;
-
         CFPropertyListRef midi_properties;
+        error = MIDIObjectGetProperties(*midi_object, &midi_properties, true);
+
+        CFNumberRef uniqueID;
+        SInt64 uid;
+        set_uid_from_properties(&midi_properties, &uniqueID, &uid);
+
+        VALUE midi_destination = rb_funcall(rb_cDestination, new_intern, 0);
+        rb_iv_set(midi_destination, "@uid", INT2FIX(uid));
+
+        return midi_destination;
+}
+
+static VALUE
+create_device_obj(MIDIDeviceRef *midi_device)
+{
+        OSStatus error;
+        CFPropertyListRef midi_properties;
+        error = MIDIObjectGetProperties(*midi_device, &midi_properties, true);
+
         CFStringRef name;
         CFStringRef driver;
         CFStringRef manufacturer;
@@ -102,37 +111,30 @@ create_device_from_properties(MIDIDeviceRef *midi_device)
 
         SInt64      uid;
         ItemCount   entities_count;
+        ItemCount   i;
 
-        error = MIDIObjectGetProperties(*midi_device, &midi_properties, true);
+        set_name_from_properties(&midi_properties, &name);
+        set_uid_from_properties(&midi_properties, &uniqueID, &uid);
 
-
-        set_name_from_properties(midi_properties, &name);
-        set_uid_from_properties(midi_properties, &uniqueID, &uid);
-
-        driver         = CFDictionaryGetValue(*midi_properties, CFSTR("driver"));
-        manufacturer   = CFDictionaryGetValue(*midi_properties, CFSTR("manufacturer"));
-        entities       = CFDictionaryGetValue(*midi_properties, CFSTR("entities"));
-
-        entities_count = MIDIDeviceGetNumberOfEntities((MIDIDeviceRef) *device);
-        for (i = 0; i < entities_count; ++i)
-        {
-                MIDIEntityRef midi_entity = MIDIDeviceGetEntity(midi_device, (ItemCount)i);
-
-                MIDIObjectGetProperties(midi_entity, &midi_properties, true);
-                VALUE entity = create_entity_from_properties(&midi_properties);
-                rb_funcall(rb_mKernel, rb_intern("puts"), rb_funcall(entity, rb_intern("inspect"), 0));
-        }
-
-
+        driver         = CFDictionaryGetValue(midi_properties, CFSTR("driver"));
+        manufacturer   = CFDictionaryGetValue(midi_properties, CFSTR("manufacturer"));
 
         VALUE midi_object = rb_funcall(rb_cDevice, new_intern, 0);
+        VALUE rb_entities = rb_ary_new();
 
         rb_iv_set(midi_object, "@name", rb_str_new2(CFStringGetCStringPtr(name, kCFStringEncodingUTF8)));
         rb_iv_set(midi_object, "@driver", rb_str_new2(CFStringGetCStringPtr(driver, kCFStringEncodingUTF8)));
         rb_iv_set(midi_object, "@manufacturer", rb_str_new2(CFStringGetCStringPtr(manufacturer, kCFStringEncodingUTF8)));
         rb_iv_set(midi_object, "@uid", INT2FIX(uid));
-        rb_iv_set(midi_object, "@entities", rb_ary_new());
+        rb_iv_set(midi_object, "@entities", rb_entities);
 
+        entities_count = MIDIDeviceGetNumberOfEntities(*midi_device);
+        for (i = 0; i < entities_count; ++i)
+        {
+                MIDIEntityRef midi_entity = MIDIDeviceGetEntity(*midi_device, i);
+                MIDIObjectGetProperties(midi_entity, &midi_properties, true);
+                rb_ary_push(rb_entities, create_entity_obj(&midi_entity));
+        }
         return midi_object;
 }
 
@@ -151,13 +153,13 @@ find_by_unique_id(VALUE klass, VALUE uid)
 
         switch(*outObjectType) {
         case kMIDIObjectType_Device:
-                return create_device_from_properties(outObject);
+                return create_device_obj((MIDIDeviceRef *)outObject);
         case kMIDIObjectType_Entity:
-                return create_entity_from_properties(&midi_properties);
+                return create_entity_obj((MIDIEntityRef *)outObject);
         case  kMIDIObjectType_Source:
-                return create_source_from_properties(&midi_properties);
+                return create_source_obj((MIDIEndpointRef *)outObject);
         case kMIDIObjectType_Destination:
-                return create_destination_from_properties(&midi_properties);
+                return create_destination_obj((MIDIEndpointRef *)outObject);
         default:
                 /* raise an exception of unhandled object type  */
                 printf ("unkonwn type\n");
