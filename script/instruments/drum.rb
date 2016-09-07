68a5fe2adb23593953b60bfcd7db71e60aebf57c
yann marquet <ymarquet@gmail.com>
2016-09-07 19:07:47 +0100

    drum stuff

--- script/instruments/drum.rb
+++ script/instruments/drum.rb
@@ -1,20 +1,28 @@
 play 'C4',  [
   1.0, 0.0, 0.0, 0.0,
-  0.0, 0.0, 0.0, 0.0,
-  0.0, 0.0, 0.8, 0.2,
-  0.0, 0.0, 0.0, 0.0
+  0.0, 0.0, 0.8, 0.0,
+  0.0, 0.0, 0.0, 0,
+  0.0, 0.0, 0.0, 0
 ],
   probability_generator: -> { SecureRandom.random_number }
 
 
 play 'D4',  [
   0.0, 0.0, 0.0, 0.0,
-  1.0, 0.0, 0.0, 0.0,
-  0.0, 0.0, 0.0, 0.0,
+  1.0, 0.0, 0.2, 0.0,
+  0.0, 0.0, 0.2, 0.4,
   1.0, 0.0, 0.0, 0.0
-]
+],
+  probability_generator: -> { SecureRandom.random_number }
 
-# play 'B4',  [  0.0,  0.0,  1.0, 0.0,  0.9, 0.0,  0.0, 0.0, 0.0, 0.0, 0.0, 0.1,  0.5, 0.0,  0.5, 0.1 ], -> { SecureRandom.random_number }
+# play 'E4',  [
+#   0.0, 0.0, 0.0, 0.1,
+#   1.4, 0.2, 0.0, 0.0,
+#   0.0, 0.0, 0.0, 0.0,
+#   0.0, 0.0, 0.0, 0.0
+# ]
+
+# play 'B4',  [  0.0,  0.0,  1.0, 0.0,  0.9, 0.0,  0.0, 0.0, 0.0, 0.0, 0.0, 0.1,  0.5, 0.0,  0.5, 0.1 ], probability_generator: -> { SecureRandom.random_number }
 play 'F#4', [
   1.0, 0.2,  0.6, 0.2,
   0.4, 0.1,  0.0, 0.15,
@@ -24,4 +32,4 @@ play 'F#4', [
   probability_generator: -> { SecureRandom.random_number }
 
 play 'G#4', [  0.0,  4.0,  0.0, 0.7 ] * 4, probability_generator: -> { SecureRandom.random_number }
-# play 'C5', [  0.0,  0.0,  0.0, 0.0,  0.0, 0.0,  0.2, 0.0, 0.0, 0.9, 0.9, 0.9,  0.2, 0.0,  0.0, 0.9 ], -> { SecureRandom.random_number }
+play 'D5', [  0.0,  0.0,  0.0, 0.0,  0.0, 0.0,  0.2, 0.0, 0.0, 0.9, 0.9, 0.9,  0.2, 0.0,  0.0, 0.9 ], probability_generator: -> { SecureRandom.random_number }
